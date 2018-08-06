a501ec66e7473ea5c8bc8bc4c44d4964d54f9f3a
Scott Weinstein <sweinstein27@gmail.com>
2018-08-06 17:24:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    session[:item] = @item
+    @session = session
+    erb :checkout
+  end
+
 end
