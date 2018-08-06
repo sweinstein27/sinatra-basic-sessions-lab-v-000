490fca097612748b596a4873a2a68e5ddd790413
Scott Weinstein <sweinstein27@gmail.com>
2018-08-06 16:58:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -9,6 +10,10 @@ end
 
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+    
   end
 
 end
